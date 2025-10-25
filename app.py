88277ad20bb148d0c7979b04149f06688e36608a
Leandro <leandro.assis@allarmi.com.br>
2025-10-24 22:39:54 -0300

    app.py

--- app.py
+++ app.py
@@ -68,11 +68,7 @@ def tela_login():
       # --- 4. Coluna da Direita (Branding) ---
     with col2:
         st.markdown('<div class="login-right-container">', unsafe_allow_html=True)
-        
-        # --- IMAGEM PRINCIPAL (HEXÁGONOS) ---
-        if imagem_principal:
-            st.image(imagem_principal, use_container_width=True) 
-        
+               
         # Seu texto de boas-vindas
         st.markdown("<h2>Seja bem vindo a plataforma de gestão de projetos</h2>", unsafe_allow_html=True)
         
@@ -430,6 +426,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
