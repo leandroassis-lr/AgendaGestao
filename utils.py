bb9e20702a9ec757f256ce852656cae7f50bda12
Leandro <leandro.assis@allarmi.com.br>
2025-10-26 00:58:23 -0300

    utils.py

--- utils.py
+++ utils.py
@@ -348,7 +348,7 @@ def load_css():
 
         --muted: #6b7280;
         --card-bg: #F0F8FF;
-        --page-bg: #1C1C1C; 
+        --page-bg: #DCDCDC; 
     }
 
     /* 2. FUNDOS (Página e Sidebar) */
@@ -596,6 +596,7 @@ def calcular_sla(projeto_row, df_sla):
             return "SLA Vence Hoje!", "#FFA726"
         else:
             return f"SLA: {dias_restantes}d restantes", "#66BB6F"
+
 
 
 
