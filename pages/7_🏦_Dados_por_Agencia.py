186f2533ad1418be9d80b3d4218dcaf9630bca49
Leandro <leandro.assis@allarmi.com.br>
2025-11-14 22:29:55 -0300

    7_🏦_Dados_por_Agencia.py

--- pages/7_🏦_Dados_por_Agencia.py
+++ pages/7_🏦_Dados_por_Agencia.py
@@ -531,7 +531,7 @@ def tela_dados_agencia():
                                 
                                 sistemas_no_projeto = df_projeto.groupby('Sistema')
                                 for nome_sistema, df_sistema in sistemas_no_projeto:
-                                    st.markdown(f"## {clean_val(nome_sistema, 'N/D')}")
+                                    st.markdown(f"######## {clean_val(nome_sistema, 'N/D')}")
                                     
                                     for _, chamado_row in df_sistema.iterrows():
                                         with st.expander(f"▶️ {chamado_row['Nº Chamado']} "):
@@ -603,6 +603,7 @@ def tela_dados_agencia():
 
 # --- Ponto de Entrada ---
 tela_dados_agencia ()
+
 
 
 
