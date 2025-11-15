84b18c91d3e4a450b691e4b1cb65547a834d022d
Leandro <leandro.assis@allarmi.com.br>
2025-11-14 22:51:46 -0300

    7_🏦_Dados_por_Agencia.py

--- pages/7_🏦_Dados_por_Agencia.py
+++ pages/7_🏦_Dados_por_Agencia.py
@@ -433,7 +433,10 @@ def tela_dados_agencia():
                 with col1: st.markdown(tag_html, unsafe_allow_html=True)
                 with col2: st.markdown(f"<span style='font-size: 1.15rem; font-weight: bold;'>{nome_agencia}</span>", unsafe_allow_html=True)
                 with col3: st.markdown(urgency_text, unsafe_allow_html=True)
-                with col4: st.markdown(f"###### {analista_urgente_nome}", unsafe_allow_html=True)
+                with col4:
+                    analista_color = utils_chamados.get_color_for_name(analista_urgente_nome)
+                    analista_html = f"<span style='color: {analista_color}; font-weight: 500;'>{analista_urgente_nome}</span>"
+                    st.markdown(f"###### <br>{analista_html}", unsafe_allow_html=True)
                 with col5: st.markdown(f"**{num_projetos} {'Projetos' if num_projetos > 1 else 'Projeto'}**", unsafe_allow_html=True)
 
                 # --- Nível 2 (Serviços/Projetos) ---
@@ -603,6 +606,7 @@ def tela_dados_agencia():
 
 # --- Ponto de Entrada ---
 tela_dados_agencia ()
+
 
 
 
