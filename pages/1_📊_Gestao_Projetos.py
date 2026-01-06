4eba0a2c4e18ea73bd559e0f0483b4f4e8f0c00d
Leandro <leandro.assis@allarmi.com.br>
2026-01-05 23:40:18 -0300

    1_📊_Gestao_Projetos.py

--- pages/1_📊_Gestao_Projetos.py
+++ pages/1_📊_Gestao_Projetos.py
@@ -9,6 +9,7 @@ import math
 import io
 
 st.set_page_config(page_title="Gestão de Projetos", page_icon="📊", layout="wide")
+utils.load_css()
 
 # --- CSS ESTILO PERSONALIZADO ---
 st.markdown("""
@@ -1240,5 +1241,6 @@ else:
                         an = str(r.get('Analista', 'N/D')).split(' ')[0].upper()
                         ag = str(r.get('Cód. Agência', '')).split('.')[0]
                         st.markdown(f"""<div style="background:white; border-left:4px solid {cc}; padding:6px; margin-bottom:6px; box-shadow:0 1px 2px #eee; font-size:0.8em;"><b>{sv}</b><br><div style="display:flex; justify-content:space-between; margin-top:4px;"><span>🏠 {ag}</span><span style="background:#E3F2FD; color:#1565C0; padding:1px 4px; border-radius:3px; font-weight:bold;">{an}</span></div></div>""", unsafe_allow_html=True)
+
 
 
